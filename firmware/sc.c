0fb80d035c926e3a2ebbd410ca067367fe40c382
Petre Rodan <petre.rodan@simplex.ro>
2013-07-28 20:28:47 +0300

    save status to sd-card only if relays activate

--- firmware/sc.c
+++ firmware/sc.c
@@ -41,7 +41,16 @@ float v_bat, v_bat_old, v_pv, itemp;
 
 uint8_t first_start, last_ch_ena;
 uint8_t relay_ch_ena, relay_opt_ena;
+uint32_t status_last;
 
+// relay_acts register
+// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+//  opt deact |  opt act  | ch deact  | ch act
+// bits[0-3]   - number of charging relay activations per 1min cycle
+// bits[4-7]   - number of charging relay deactivations per 1min cycle
+// bits[8-11]  - number of opt relay activations per 1min cycle
+// bits[12-15] - number of opt relay deactivations per 1min cycle
+//
 // acts register
 // 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
 //                                   |  |  |  |  |
@@ -50,15 +59,8 @@ uint8_t relay_ch_ena, relay_opt_ena;
 //                                   |  |   --------> start up the charge controller
 //                                   |   -----------> stop the charge to read v_pv
 //                                    --------------> enable charging
-// relay_acts register
-// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-//  opt deact |  opt act  | ch deact  | ch act
-// bits[0-3]   - number of charging relay activations per 1min cycle
-// bits[4-7]   - number of charging relay deactivations per 1min cycle
-// bits[8-11]  - number of opt relay activations per 1min cycle
-// bits[12-15] - number of opt relay deactivations per 1min cycle
 
-uint16_t relay_acts, acts;
+uint16_t acts, relay_acts;
 
 void die(uint8_t loc, FRESULT rc)
 {
@@ -79,15 +81,12 @@ static void do_calib(enum sys_message msg)
     itemp = ((q_itemp * VREF_1_5) / 102.3 - 6.88) * 396.8;
     adc10_halt();
 
-/*
--21.3 | bat 1023 22.22 | pv 1023 22.22DA0
-*/
-    snprintf(str_temp, 42, "% 2d.%1d | bat % 4d %02d.%02d | pv % 4d %02d.%02d\r\n",
-                        (uint16_t) itemp / 10, (uint16_t) itemp % 10,
-                        q_bat,
-                        (uint16_t) v_bat / 100, (uint16_t) v_bat % 100, 
-                        q_pv,
-                        (uint16_t) v_pv / 100, (uint16_t) v_pv % 100);
+    //-21.3 | bat 1023 22.22 | pv 1023 22.22DA0
+    snprintf(str_temp, 42,
+             "% 2d.%1d | bat % 4d %02d.%02d | pv % 4d %02d.%02d\r\n",
+             (uint16_t) itemp / 10, (uint16_t) itemp % 10, q_bat,
+             (uint16_t) v_bat / 100, (uint16_t) v_bat % 100, q_pv,
+             (uint16_t) v_pv / 100, (uint16_t) v_pv % 100);
     uart_tx_str(str_temp, strlen(str_temp));
 
 }
@@ -96,6 +95,7 @@ static void do_smth(enum sys_message msg)
 {
     uint16_t q_bat = 0;
     uint16_t q_pv = 0, q_itemp = 0;
+    uint32_t status;
 
     relay_acts = 0;
     acts = 0;
@@ -113,7 +113,6 @@ static void do_smth(enum sys_message msg)
         charge_disable();
         acts |= BIT0;
     }
-
     // values are multiplied by 100 for snprintf
     if (v_bat < 300) {
         // do nothing since we run on the 3v Li cell
@@ -122,19 +121,18 @@ static void do_smth(enum sys_message msg)
     } else if (v_bat > 1410) {
         charge_disable();
         acts |= BIT1;
-    } else if ((!relay_ch_ena) && (last_ch_ena != rtca_time.hour) && (!first_start)) {
+    } else if ((!relay_ch_ena) && (last_ch_ena != rtca_time.hour)) {
         // once an hour give some juice to the charge controller
         charge_enable();
         timer_a0_delay(50000);
         charge_disable();
         acts |= BIT2;
     }
-
     // once a while switch off the charge relay so we can read the PV voltage
-    if (relay_ch_ena && ((rtca_time.min == 0) || (v_bat_old - v_bat > 10))) {
+    if (relay_ch_ena && ((rtca_time.min % 10 == 1) || (v_bat_old - v_bat > 10))) {
         charge_disable();
-        // wait for cap to charge
-        timer_a0_delay(5000);
+        // wait for cap to discharge
+        timer_a0_delay(1000000);
         acts |= BIT3;
     }
 
@@ -153,52 +151,58 @@ static void do_smth(enum sys_message msg)
         }
     }
 
-    FRESULT rc;
-    f_mount(0, &fatfs);
+    status = relay_acts + ((uint32_t) acts << 16) + ((uint32_t) relay_ch_ena << 24) + ((uint32_t) relay_opt_ena << 25);
 
-    opt_power_enable();
-    timer_a0_delay(50000);
-    disk_initialize(0);
+    if (status != status_last) {
 
-    rc = detectCard();
+        FRESULT rc;
+        f_mount(0, &fatfs);
 
-    if (rc) {
-        uint16_t bw;
-        f_opendir(&dir, "/");
-        snprintf(str_temp, 5, "%d", rtca_time.year);
-        rc = f_mkdir(str_temp);
-        if (( rc == FR_OK) || ( rc == FR_EXIST)) {
-            snprintf(str_temp, 9, "/%d/%02d", rtca_time.year, rtca_time.mon);
-            rc = f_open(&f, str_temp, FA_WRITE | FA_OPEN_ALWAYS);
-            if (!rc) {
-                f_lseek(&f, f_size(&f));
-                //20130620 07:12 -34.5 12.30 13.40 1 1 0xffff 0xffff__0
-                snprintf(str_temp, 53, "%04d%02d%02d %02d:%02d % 2d.%1d %02d.%02d %02d.%02d %d %d 0x%04x 0x%04x\r\n",
-                        rtca_time.year, rtca_time.mon, rtca_time.day,
-                        rtca_time.hour, rtca_time.min,
-                        (uint16_t) itemp / 10, (uint16_t) itemp % 10,
-                        (uint16_t) v_bat / 100, (uint16_t) v_bat % 100, 
-                        (uint16_t) v_pv / 100, (uint16_t) v_pv % 100,
-                        relay_ch_ena, relay_opt_ena, relay_acts, acts);
-                f_write(&f, str_temp, strlen(str_temp), &bw);
-                f_close(&f);
-                uart_tx_str(str_temp, strlen(str_temp));
+        opt_power_enable();
+        timer_a0_delay(50000);
+        disk_initialize(0);
+
+        rc = detectCard();
+
+        if (rc) {
+            uint16_t bw;
+            f_opendir(&dir, "/");
+            snprintf(str_temp, 5, "%d", rtca_time.year);
+            rc = f_mkdir(str_temp);
+            if ((rc == FR_OK) || (rc == FR_EXIST)) {
+                snprintf(str_temp, 9, "/%d/%02d", rtca_time.year, rtca_time.mon);
+                rc = f_open(&f, str_temp, FA_WRITE | FA_OPEN_ALWAYS);
+                if (!rc) {
+                    f_lseek(&f, f_size(&f));
+                    //20130620 07:12 -34.5 12.30 13.40 1 1 0xffff 0xffffDA0
+                    snprintf(str_temp, 53,
+                             "%04d%02d%02d %02d:%02d % 2d.%1d %02d.%02d %02d.%02d %d %d 0x%04x 0x%04x\r\n",
+                             rtca_time.year, rtca_time.mon, rtca_time.day,
+                             rtca_time.hour, rtca_time.min,
+                             (uint16_t) itemp / 10, (uint16_t) itemp % 10,
+                             (uint16_t) v_bat / 100, (uint16_t) v_bat % 100,
+                             (uint16_t) v_pv / 100, (uint16_t) v_pv % 100,
+                             relay_ch_ena, relay_opt_ena, relay_acts, acts);
+                    f_write(&f, str_temp, strlen(str_temp), &bw);
+                    f_close(&f);
+                    uart_tx_str(str_temp, strlen(str_temp));
+                } else {
+                    die(2, rc);
+                }
             } else {
-                die(2, rc);
+                die(3, rc);
             }
         } else {
-                die(3, rc);
+            die(1, rc);
         }
-    } else {
-        die(1, rc);
+        f_mount(0, NULL);
+        SDCard_end();
+        opt_power_disable();
+        status_last = status;
     }
-    f_mount(0, NULL);
-    SDCard_end();
-    opt_power_disable();
-
     //P4OUT ^= BIT7;              // blink led
 }
-#endif // !CALIBRATION
+#endif                          // !CALIBRATION
 
 void check_ir(void)
 {
@@ -308,7 +312,7 @@ void main_init(void)
     PJOUT = 0x00;
     PJDIR = 0xFF;
 
-    /*
+#ifdef CALIBRATION
     // send MCLK to P4.0
     __disable_interrupt();
     // get write-access to port mapping registers
@@ -322,18 +326,17 @@ void main_init(void)
     __enable_interrupt();
     P4DIR |= BIT0;
     P4SEL |= BIT0;
-    */
 
-    /*
     // send ACLK to P1.0
     P1DIR |= BIT0;
     P1SEL |= BIT0;
-    */
+#endif
 
     relay_ch_ena = false;
     relay_opt_ena = false;
     first_start = true;
     last_ch_ena = 0;
+    status_last = -1L;
     v_bat = 0;
 
     rtca_init();
